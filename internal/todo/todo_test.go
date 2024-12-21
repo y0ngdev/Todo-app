b21e317ea1e74ab57bd5627dbd14dbbd3b60956b
Prosper Ugbovo [YONGDEV] <85100001+prosperyong@users.noreply.github.com>
2024-12-21 01:07:33 +0100

    progress

--- internal/todo/todo_test.go
+++ internal/todo/todo_test.go
@@ -6,24 +6,33 @@ import (
 )
 
 func TestService_Search(t *testing.T) {
-	type fields struct {
-		todos []Item
-	}
+
 	type args struct {
 		query string
 	}
 	tests := []struct {
 		name   string
-		fields fields
+		fields []string
 		args   args
 		want   []string
 	}{
 		// TODO: Add test cases.
+		{
+			name:   "Given a todo of shop and a search of sh, i should get shop back",
+			fields: []string{"shop"},
+			args:   args{"sh"},
+			want:   []string{"shop"},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			svc := &Service{
-				todos: tt.fields.todos,
+			svc := NewService()
+
+			for _, add := range tt.fields {
+				err := svc.Add(add)
+				if err != nil {
+					t.Error(err)
+				}
 			}
 			if got := svc.Search(tt.args.query); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Search() = %v, want %v", got, tt.want)
